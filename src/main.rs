732b0e89ece29e1e4c7454724e1d96635be8fedc
Juha Lepola <juha.lepola@gmail.com>
2017-10-19 21:22:21 +0300

    Automated test runner
    Prints results in Markdown table.
    
    TODO: Run the tests multiple times and report averages.

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,9 @@ extern crate memmap;
 extern crate rayon;
 
 
-use std::fs::File;
 use std::io::BufWriter;
-use std::io::Result;
+use std::path::Path;
 use std::io::prelude::*;
-use std::time;
 use std::slice;
 use std::collections::HashSet;
 use std::iter::FromIterator;
@@ -20,7 +18,6 @@ use memmap::{Mmap, Protection};
 use rand::distributions::{IndependentSample, Range};
 
 use docopt::Docopt;
-use ro_scalar_set::RoScalarSet;
 use rayon::prelude::*;
 
 const USAGE: &'static str = "
@@ -29,6 +26,7 @@ Scalar Set Evaluator.
 Usage:
   scalar_set_eval new <file> <minvalue> <maxvalue> <values> <sets>
   scalar_set_eval eval <file> <minvalue> <maxvalue> <values> [<sets>]
+  scalar_set_eval test <report> <minvalue> <maxvalue> [<values>] [<sets>]
   scalar_set_eval (-h | --help)
   scalar_set_eval --version
 ro_scalar_set
@@ -41,6 +39,7 @@ Options:
 #[derive(Debug, Deserialize)]
 struct Args {
     arg_file: String,
+    arg_report: String,
     arg_minvalue: i32,
     arg_maxvalue: i32,
     arg_sets: i32,
@@ -49,6 +48,23 @@ struct Args {
     flag_mt: bool,
     cmd_new: bool,
     cmd_eval: bool,
+    cmd_test: bool,
+}
+
+/// Results of a single test.
+///  # Members
+/// * set_size Number of values in a set.
+/// * set_count Number of sets
+/// * test_set_size Number of values in the test set
+/// * duration The length of the evaluation
+/// * matches The number of sets that have a value matching with a value in the test set.
+struct TestResult {
+  set_size: i32,
+  set_count: i32,
+  test_set_size: i32,
+
+  duration: std::time::Duration,
+  matches: u32,
 }
 
 fn main() {
@@ -56,18 +72,121 @@ fn main() {
                             .and_then(|d| d.deserialize())
                             .unwrap_or_else(|e| e.exit());
 
-  // Generate new file?Launch
+  // Determine action.
    let start = std::time::Instant::now();
   if args.cmd_new {
     generate( &args.arg_file, args.arg_sets, args.arg_values, args.arg_minvalue, args.arg_maxvalue );
   }
   else if args.cmd_eval {
-    let match_count = evaluate( &args.arg_file, args.arg_sets, args.arg_values, args.arg_minvalue, args.arg_maxvalue );
-    println!("Found {} matches", match_count );
+    let ( match_count, duration ) = evaluate( &args.arg_file, args.arg_values, args.arg_minvalue, args.arg_maxvalue );
+    println!("Found {} matches in {}.{:06} s", match_count, duration.as_secs(), duration.subsec_nanos() / 1000 );
+  }
+  else if args.cmd_test {
+    test( &args.arg_report, args.arg_minvalue, args.arg_maxvalue );
+  }
+  else {
+    println!("{}", "No tests selected." );
   }
   let stop = std::time::Instant::now();
   let duration = stop.duration_since( start );
-  println!("Operation took {}.{} s.",duration.as_secs(), duration.subsec_nanos() );
+  println!("Operation took {}.{:06} s.",duration.as_secs(), duration.subsec_nanos() / 1000 );
+}
+
+
+fn test(
+  report: &String,
+  min_value: i32,
+  max_value: i32,
+)
+{
+  // Define test material.
+  // let set_sizes: Vec<i32> = vec! { 10, 100, 1000 };
+  // let set_counts: Vec<i32> = vec! { 10, 100, 1000  };
+  let set_sizes: Vec<i32> = vec! { 10, 100, 1000, 10000 };
+  let set_counts: Vec<i32> = vec! { 10, 100, 1000, 10000, 100000 };
+  let test_set_sizes: Vec<i32> = vec! { 10, 100, 1000, 10000 };
+
+  // Generate test files.
+  for set_size in &set_sizes {
+    for set_count in &set_counts {
+
+      // Reuse existing files if available.
+      let file_name = format!( "{}_sets_with_{}_values.bin", set_count, set_size,  );
+      if Path::new( &file_name ).exists() {
+        continue;
+      }
+
+      println!("Generating test set {}...", file_name );
+      generate( &file_name, *set_count, *set_size, min_value, max_value );
+    }
+  }
+
+  // Run the tests.
+  let mut results: Vec<TestResult> = Vec::new();
+  for set_size in &set_sizes {
+    for set_count in &set_counts {
+      for test_set_size in &test_set_sizes {
+
+        // Identify the current test.
+        let file_name = format!( "{}_sets_with_{}_values.bin", set_count, set_size,  );
+        if ! Path::new( &file_name ).exists() {
+          panic!( "Generated file not found." );
+        }
+
+        // Run and measure.
+        println!("Running test set {}...", file_name );
+        let ( match_count, duration ) = evaluate( &file_name, *test_set_size, min_value, max_value );
+
+        // Collect results for reporting.
+        let result = TestResult {
+            set_size: *set_size, set_count: *set_count,
+            test_set_size: *test_set_size,
+            duration: duration, matches: match_count  };
+        results.push( result );
+      }
+    }
+  }
+
+  // Report the results.
+  let report = std::fs::File::create( report ).expect( "Failed to open the report." );
+  let mut report = BufWriter::with_capacity( 1024 * 1024, report );
+
+  let mut current_set_size = results[ 0 ].set_size;
+  let mut write_header: bool = true;
+  for result in &results {
+
+    // Always write header when we ancounter a new set size.
+    if result.set_size != current_set_size {
+      write_header = true;
+      current_set_size = result.set_size;
+    }
+
+    // Header?
+    if write_header {
+
+      writeln!( &mut report, "" ).expect( "Writing report failed." );
+      writeln!( &mut report,
+          "Number of values in a set: {}", current_set_size )
+          .expect( "Writing report failed." );;
+      writeln!( &mut report,
+          "|{:14}|{:14}|{:14}|{:14}|",
+          "Sets", "Test set size", "Matching sets", "Duration" ).expect( "Writing report failed." );
+      writeln!( &mut report,
+          "|{:-<14}|{:-<14}|{:-<14}|{:-<14}|",
+          "-", "-", "-", "-" ).expect( "Writing report failed." );
+
+      write_header = false;
+    }
+
+    // Report results of a single test.
+    writeln!( &mut report,
+        "|{:14}|{:14}|{:14}|{:5}.{:06} s|",
+        result.set_count, result.test_set_size, result.matches,
+        result.duration.as_secs(), result.duration.subsec_nanos() / 1000 )
+        .expect( "Writing report failed." );
+
+  }
+
 }
 
 fn generate(
@@ -104,15 +223,13 @@ fn generate(
 /// Evaluates integer sets.
 fn evaluate<'a>(
   file: &String,
-  set_count: i32,
   values_in_set: i32,
   min_value: i32,
   max_value: i32
-) -> u32
+) -> ( u32, std::time::Duration )
 {
   // Construct test vector.
   let between = Range::new( min_value, max_value );
-  let mut rng = rand::thread_rng();
   let test_set = generate_values( values_in_set, &between );
 
   // Open file for reading.
@@ -131,8 +248,7 @@ fn evaluate<'a>(
       let match_counter = sets.par_iter().map( |s| evaluate_set( test_set.as_slice(), &s ) ).sum();      
       let stop = std::time::Instant::now();
       let duration = stop.duration_since( start );
-      println!("Set evaluation took {}.{} s.",duration.as_secs(), duration.subsec_nanos() );
-      return match_counter;
+      return ( match_counter, duration );
     }   
   }
 }
@@ -145,14 +261,13 @@ fn evaluate_set(
 ) -> u32 {
 
     // Test if any of values in the set are found from the current scalar set.
-    let mut match_counter: u32 = 0;
     for t in test_set
     {
       if set.contains( *t ) {
-        match_counter += 1;
+        return 1;
       }
     }
-    return match_counter;
+    return 0;
 }
 
 fn generate_values(
